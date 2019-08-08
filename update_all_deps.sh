d4d958eb9641d42837ebccf5b952fcad130103eb
Ben Kolera <ben.kolera@gmail.com>
2019-08-08 11:32:46 +1000

    chore: bump everything

--- update_all_deps.sh
+++ update_all_deps.sh
@@ -3,3 +3,5 @@
 ./home-manager/home-overlays/lorri/update.sh
 ./home-manager/home-overlays/spacemacs/update.sh
 ./home-manager/home-overlays/taffybar/update.sh
+./home-manager/home-overlays/tmux-themepack/update.sh
+./home-manager/home-overlays/kak-fzf/update.sh
