686dbf316584ca18b36d70da80039e2fa4482775
Ben Kolera <ben.kolera@gmail.com>
2019-08-25 15:42:54 +1000

    feat: Add postgres query logging

--- default.nix
+++ default.nix
@@ -126,7 +126,14 @@ in {
     openssh.enable = true;
     printing.enable = true;
     devmon.enable = true;
-    postgresql.enable = true;
+    postgresql = {
+      enable = true;
+      extraConfig = ''
+        log_directory = '/tmp/pg_log'                    
+        log_filename = 'postgresql-%Y-%m-%d_%H%M%S.log'
+        log_statement = 'all'
+      '';
+    };
     dbus.packages = [ pkgs.blueman ];
     smokeping = {
       enable = true;
