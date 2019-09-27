6c6dcb70244907fe2a93447163d5283b60065b8e
Ben Kolera <ben.kolera@gmail.com>
2019-09-27 13:05:12 +1000

    fix: Adds tig too

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -221,6 +221,54 @@ in {
         }
         exec '<a-;><a-X>'
       }
+      def suspend-and-resume \
+          -params 1..2 \
+          -docstring 'suspend-and-resume <cli command> [<kak command after resume>]: backgrounds current kakoune client and runs specified cli command.  Upon exit of command the optional kak command is executed.' \
+          %{ evaluate-commands %sh{
+
+          # Note we are adding '&& fg' which resumes the kakoune client process after the cli command exits
+          cli_cmd="$1 && fg"
+          post_resume_cmd="$2"
+
+          # automation is different platform to platform
+          platform=$(uname -s)
+          case $platform in
+              Darwin)
+                  automate_cmd="sleep 0.01; osascript -e 'tell application \"System Events\" to keystroke \"$cli_cmd\\n\" '"
+                  kill_cmd="/bin/kill"
+                  break
+                  ;;
+              Linux)
+                  automate_cmd="sleep 0.2; xdotool type '$cli_cmd'; xdotool key Return"
+                  kill_cmd="/usr/bin/kill"
+                  break
+                  ;;
+          esac
+
+          # Uses platforms automation to schedule the typing of our cli command
+          nohup sh -c "$automate_cmd"  > /dev/null 2>&1 &
+          # Send kakoune client to the background
+          $kill_cmd -SIGTSTP $kak_client_pid
+
+          # ...At this point the kakoune client is paused until the " && fg " gets run in the $automate_cmd
+
+          # Upon resume, run the kak command is specified
+          if [ ! -z "$post_resume_cmd" ]; then
+              echo "$post_resume_cmd"
+          fi
+      }}
+      def tig-blame -override -docstring 'Open blame in tig for current file and line' %{
+          # Note here we aren't passing any command on resume of kakoune
+          suspend-and-resume "tig blame +%val{cursor_line} %val{buffile}" 
+      }
+
+      declare-user-mode tig
+      map global tig b ': tig-blame<ret>' -docstring 'show blame (with tig)'
+      map global tig s ': suspend-and-resume "tig status"<ret>' -docstring 'show git status (with tig)'
+      map global tig m ': suspend-and-resume "tig"<ret>' -docstring 'show main view (with tig)'
+
+      map global user t ': enter-user-mode tig<ret>' -docstring 'tig commands'
+
       def for-each-line \
           -docstring "for-each-line <command> <path to file>: run command with the value of each line in the file" \
           -params 2 \
