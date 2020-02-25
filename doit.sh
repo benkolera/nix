ca7dd45e3660c5e0e6f6eaa85e0be516e8925d2b
Ben Kolera <ben.kolera@gmail.com>
2020-02-25 15:56:01 +1000

    kak master

--- doit.sh
+++ doit.sh
@@ -1,4 +1,4 @@
-if [ $1 == "amend" ]; then
+if [[ $1 == "amend" ]]; then
 git add -A && git commit --amend && git push -f && sudo ./update.sh && sudo nixos-rebuild switch
 else
 git add -A && git commit && git push && sudo ./update.sh && sudo nixos-rebuild switch
--- home-manager/home-overlays/kakoune/default.nix
+++ home-manager/home-overlays/kakoune/default.nix
@@ -4,6 +4,37 @@ let
   kakoune-src = self.fetchgit {
     inherit (gitinfo) url rev sha256;
   };
+  kakoune-unwrapped = stdenv.mkDerivation rec {
+    pname = "kakoune-unwrapped";
+    version = "2020.01.16";
+    src = kakoune-src; 
+    nativeBuildInputs = [ pkgconfig ];
+    buildInputs = [ ncurses asciidoc docbook_xsl libxslt ];
+    makeFlags = [ "debug=no" ];
+
+    postPatch = ''
+      export PREFIX=$out
+      cd src
+      sed -ie 's#--no-xmllint#--no-xmllint --xsltproc-opts="--nonet"#g' Makefile
+    '';
+
+    preConfigure = ''
+      export version="v${version}"
+    '';
+
+    doInstallCheckPhase = true;
+    installCheckPhase = ''
+      $out/bin/kak -ui json -E "kill 0"
+    '';
+
+    meta = {
+      homepage = http://kakoune.org/;
+      description = "A vim inspired text editor";
+      license = licenses.publicDomain;
+      maintainers = with maintainers; [ vrthra ];
+      platforms = platforms.unix;
+    };
+  };
 in {
-  kakoune = kakoune-src;
+  kakoune = super.wrapKakoune kakoune-unwrapped;
 }
