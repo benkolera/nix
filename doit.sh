dce9ae9a72c8d3151a523a7781c20463e26cf61d
Ben Kolera <ben.kolera@gmail.com>
2020-02-25 14:06:52 +1000

    fix: needs gtk3-x11

--- doit.sh
+++ doit.sh
@@ -1,1 +1,5 @@
+if [ $1 == "amend" ]; then
+git add -A && git commit --amend && git push -f && sudo ./update.sh && sudo nixos-rebuild switch
+else
 git add -A && git commit && git push && sudo ./update.sh && sudo nixos-rebuild switch
+fi
--- home-manager/home-overlays/haskell-gtk.nix
+++ home-manager/home-overlays/haskell-gtk.nix
@@ -1,7 +1,9 @@
 self: super: {
   haskellPackages = super.haskellPackages.override (old: {
     overrides = super.lib.composeExtensions (old.overrides or (_: _: {})) (hself: hsuper: {
-      gi-gdkx11 = hself.callHackage "gi-gdkx11" "3.0.9" { inherit (self) gtk; };
+      gi-gdkx11 = self.haskell.lib.overrideCabal (hself.callHackage "gi-gdkx11" "3.0.9" {}) (drv: {
+        libraryPkgconfigDepends = [ self.gtk3-x11 ];
+      });
     });
   });
 }
