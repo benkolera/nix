6af02c7074e5c559bde7351c5a525897ba55224d
Ben Kolera <ben.kolera@gmail.com>
2020-02-25 13:48:30 +1000

    fix: try this

--- home-manager/home-overlays/haskell-gtk.nix
+++ home-manager/home-overlays/haskell-gtk.nix
@@ -1,7 +1,7 @@
 self: super: {
   haskellPackages = super.haskellPackages.override (old: {
     overrides = super.lib.composeExtensions (old.overrides or (_: _: {})) (hself: hsuper: {
-      gi-gdkx11 = hself.callHackage "gi-gdkx11" "3.0.9" {};
+      gi-gdkx11 = hself.callHackage "gi-gdkx11" "3.0.9" { self.gtk };
     });
   });
 }
