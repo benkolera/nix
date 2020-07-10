1ab33dc0d174197adda23164c1d1a6823c763b9b
Ben Kolera <ben.kolera@gmail.com>
2020-07-10 19:22:40 +1000

    Adding hexacore xmobar

--- home-manager/dotfiles/xmonad/xmobar.hs
+++ home-manager/dotfiles/xmonad/xmobar.hs
@@ -8,7 +8,7 @@ Config {
     allDesktops = True,
     persistent = False,
     commands = [
-          Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10
+          Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3> <total4> <total 5>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10
         , Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10
         , Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10
         , Run Date "%a %b %_d %H:%M" "date" 10
