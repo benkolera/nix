cc1fc4f021e390e640427140480233925de6c69b
Ben Kolera <ben.kolera@gmail.com>
2020-07-10 19:46:29 +1000

    screen desktop

--- home-manager/scripts/default.nix
+++ home-manager/scripts/default.nix
@@ -22,6 +22,13 @@ rec {
       --output DP-6 --off
   '';
 
+  screen-desktop = writeScriptBin "screen-desktop" ''
+    ${xorg.xrandr}/bin/xrandr \
+      --output HDMI-1 --mode 1920x1080 --pos 0x394 --rotate normal \
+      --output DP-1 --primary --mode 1920x1080 --pos 1920x394 --rotate normal \
+      --output DP-2 --mode 1920x1080 --pos 3840x0 --rotate right \
+  '';
+
   screen-laptop = writeScriptBin "screen-laptop" ''
     ${xorg.xrandr}/bin/xrandr \
       --output DP-4 --mode 3840x2160 --scale 0.5x0.5 --primary \
@@ -37,5 +44,5 @@ rec {
   emcf = writeScriptBin "emcf" ''${emacs}/bin/emacsclient -c $@'';
   emct = writeScriptBin "emct" ''${emacs}/bin/emacsclient -t $@'';
   pa-speakers-toggle-mute = writeScriptBin "pa-mute" ''${pulseaudio}/bin/pactl set-sink-mute 0 toggle'';
-  all = [ batman dunst-pause dunst-resume screen-laptop screen-home emcf emct pa-speakers-toggle-mute ];
+  all = [ batman dunst-pause dunst-resume screen-laptop screen-home screen-desktop emcf emct pa-speakers-toggle-mute ];
 }
