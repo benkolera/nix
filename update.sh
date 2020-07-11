61d7388bb71c6bb47e30a0d3dae5e95ac024f13d
Ben Kolera <ben.kolera@gmail.com>
2020-07-12 07:45:52 +1000

    bootstrap

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -p nix-prefetch-git -i bash
-nix-prefetch-git https://github.com/benkolera/nix $1 > /etc/nixos/thunk.json
+path=${1:-/etc/nixos/thunk.json}
+nix-prefetch-git https://github.com/benkolera/nix $1 > $path
