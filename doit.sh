b923432da141c7cf41e23a10e86adf9405c36abf
Ben Kolera <ben.kolera@gmail.com>
2020-08-26 08:52:05 +1000

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,5 +1,5 @@
 if [[ $1 == "amend" ]]; then
-git add -A && git commit --amend && git push -f && sudo ./update.sh && sudo nixos-rebuild switch
+git add -A && git commit --amend && git push -f && sudo ./update.sh
 else
-git add -A && git commit && git push && sudo ./update.sh && sudo nixos-rebuild switch
+git add -A && git commit && git push && sudo ./update.sh
 fi
