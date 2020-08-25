07a9a6ff78dd4e3efcc58a232007cf523aea4176
Ben Kolera <ben.kolera@gmail.com>
2020-08-26 08:51:38 +1000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -p nix-prefetch-git -i bash
+set -e
 path=${1:-/etc/nixos/thunk.json}
 nix-prefetch-git https://github.com/benkolera/nix $1 > $path
+nixos-rebuild switch
